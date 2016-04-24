02b5da18c4a497f5fd36fb53ad193dfd6ffff0df
Carlos Torres luna <Carlos Torres luna>
2016-04-24 12:27:26 -0500

    commit index.html and morris-data.js

--- startboos/js/morris-data.js
+++ startboos/js/morris-data.js
@@ -3,60 +3,83 @@ $(function() {
     Morris.Area({
         element: 'morris-area-chart',
         data: [{
-            period: '2010 Q1',
-            iphone: 2666,
-            ipad: null,
-            itouch: 2647
+            period: '1',
+            Acumulado: 18344.33,
+           
+            
         }, {
-            period: '2010 Q2',
-            iphone: 2778,
-            ipad: 2294,
-            itouch: 2441
+            period: '2',
+            Acumulado: 37342.33,
+          
+           
         }, {
-            period: '2010 Q3',
-            iphone: 4912,
-            ipad: 1969,
-            itouch: 2501
+            period: '3',
+            Acumulado: 57015.40,
+           
+           
         }, {
-            period: '2010 Q4',
-            iphone: 3767,
-            ipad: 3597,
-            itouch: 5689
+            period: '4',
+            Acumulado: 59010,
+            
+           
         }, {
-            period: '2011 Q1',
-            iphone: 6810,
-            ipad: 1914,
-            itouch: 2293
+            period: '5',
+            Acumulado: 61076.32,
+           
+           
         }, {
-            period: '2011 Q2',
-            iphone: 5670,
-            ipad: 4293,
-            itouch: 1881
+            period: '6',
+            Acumulado: 66213.99,
+           
+            
         }, {
-            period: '2011 Q3',
-            iphone: 4820,
-            ipad: 3795,
-            itouch: 1588
+            period: '7',
+            Acumulado: 86913.95,
+            
+           
         }, {
-            period: '2011 Q4',
-            iphone: 15073,
-            ipad: 5967,
-            itouch: 5175
+            period: '8',
+            Acumulado: 108350.14,
+            
+          
         }, {
-            period: '2012 Q1',
-            iphone: 10687,
-            ipad: 4460,
-            itouch: 2028
+            period: '9',
+            Acumulado: 130548.75,
+           
+           
         }, {
-            period: '2012 Q2',
-            iphone: 8432,
-            ipad: 5713,
-            itouch: 1791
+            period: '10',
+            Acumulado: 153536.89,
+        }, {
+
+            period: '11',
+            Acumulado: 177342.66,
+
+
+        }, {
+            period: '12',
+            Acumulado: 201995.12,
+
+
+        }, {
+            period: '13',
+            Acumulado: 227524.40,
+
+
+        }, {
+            period: '14',
+            Acumulado: 253223.68,
+
+
+        }, {
+            period: '15',
+            Acumulado: 281339.25,
+           
         }],
         xkey: 'period',
-        ykeys: ['iphone', 'ipad', 'itouch'],
-        labels: ['iPhone', 'iPad', 'iPod Touch'],
-        pointSize: 2,
+        ykeys: ['Acumulado'],
+        labels: ['Acumulado'],
+        pointSize: 5,
         hideHover: 'auto',
         resize: true
     });
